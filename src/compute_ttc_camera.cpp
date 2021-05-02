442c648dff134e8a6dc16cc81a2845cbb619d439
AnastaciaVolkova <polarbolide@gmail.com>
2021-05-02 14:23:02 +0200

    feat: Create ttc camera exercise

--- src/compute_ttc_camera.cpp
+++ src/compute_ttc_camera.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 // Purpose: Compute time-to-collision (TTC) based on keypoint correspondences in successive images
-// Notes: 
+// Notes:
 // - please take a look at the main()-function first
 // - kptsPrev and kptsCurr are the input keypoint sets, kptMatches are the matches between the two sets,
 //   frameRate is required to compute the delta time between frames and TTC will hold the result of the computation
@@ -61,14 +61,22 @@ void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPo
     TTC = -dT / (1 - meanDistRatio);
 
     // TODO: STUDENT TASK (replacement for meanDistRatio)
+    std::sort(distRatios.begin(), distRatios.end());
+    double medianDistRatio;
+    if (distRatios.size()%2 == 0)
+      medianDistRatio = (distRatios[distRatios.size()/2] + distRatios[(distRatios.size()+1)/2])/2.0;
+    else
+      medianDistRatio = distRatios[(distRatios.size()+1)/2];
+
+    TTC = -dT / (1-medianDistRatio);
 }
 
 int main()
 {
     // step 1: read pre-recorded keypoint sets from file
     // Note that the function "readKeypoints" is a helper function that is able to read pre-saved results from disk
-    // so that you can focus on TTC computation based on a defined set of keypoints and matches. 
-    // The task you need to solve in this example does not require you to look into the data structures.  
+    // so that you can focus on TTC computation based on a defined set of keypoints and matches.
+    // The task you need to solve in this example does not require you to look into the data structures.
     vector<cv::KeyPoint> kptsSource, kptsRef;
     readKeypoints("../dat/C23A5_KptsSource_AKAZE.dat", kptsSource); // readKeypoints("./dat/C23A5_KptsSource_SHI-BRISK.dat"
     readKeypoints("../dat/C23A5_KptsRef_AKAZE.dat", kptsRef); // readKeypoints("./dat/C23A5_KptsRef_SHI-BRISK.dat"
@@ -76,9 +84,9 @@ int main()
     // step 2: read pre-recorded keypoint matches from file
     vector<cv::DMatch> matches;
     readKptMatches("../dat/C23A5_KptMatches_AKAZE.dat", matches); // readKptMatches("./dat/C23A5_KptMatches_SHI-BRISK.dat", matches);
-    
+
     // step 3: compute the time-to-collision based on the pre-recorded data
-    double ttc; 
+    double ttc;
     computeTTCCamera(kptsSource, kptsRef, matches, 10.0, ttc);
     cout << "ttc = " << ttc << "s" << endl;
 }
